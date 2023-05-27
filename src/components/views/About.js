f0bcc07bcd926aba69c02695af3e5399173f3dbc
Radheshyam Soni <geekyradhe11@gmail.com>
2023-05-28 00:17:50 +0530

    Updated :construction: design of About section

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -3,10 +3,28 @@ import './About.css';
 
 const About = () => {
     return <div className='about'>
-        <h2>HELLO THERE! THIS IS RADHESHYAM SONI</h2>
-        <p>
-            As a computer science engineering student, I am passionate about technology and its ability to solve complex problems. Through my academic coursework and personal projects, I have gained experience in various programming languages and software development tools. This portfolio showcases my skills and demonstrates my ability to tackle real-world challenges with creativity and innovation.
-        </p>
+        <div className="userinfo">
+            <h2 className='my-heading'>HELLO THERE! THIS IS RADHESHYAM SONI</h2>
+            <div className='my-info'>
+                As a computer science engineering student, I am passionate about technology and its ability to solve complex problems. Through my academic coursework and personal projects, I have gained experience in various programming languages and software development tools. This portfolio showcases my skills and demonstrates my ability to tackle real-world challenges with creativity and innovation.
+            </div>
+        </div>
+        <div className="divider"></div>
+        <div className="skills">
+            <h3>Technical Skills</h3>
+            <div className='technical-skill-list'>
+            <div className="skill-container">Java</div>
+            <div className="skill-container">HTML</div>
+            <div className="skill-container">CSS</div>
+            <div className="skill-container">JavaScript</div>
+            <div className="skill-container">React</div>
+            <div className="skill-container">C++</div>
+            <div className="skill-container">Python</div>
+            <div className="skill-container">Git</div>
+            <div className="skill-container">MySQL</div>
+            <div className="skill-container">Android Studio</div>
+            </div>
+        </div>
     </div>
 }
 
