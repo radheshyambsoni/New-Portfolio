08eca6c19fb2d5d3e30dcb8fa52d5547bec289cf
Radheshyam Soni <geekyradhe11@gmail.com>
2023-06-27 23:42:39 +0530

    chore: :tada: started working on dark mode

--- src/components/Main/Navbar.js
+++ src/components/Main/Navbar.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
-import classes from './Navbar.module.css'
-import About from '../views/About/About'
-import Projects from '../views/Projects/Projects'
-import Skills from '../views/Skills/Skills'
+import React, { useState } from 'react';
+import classes from './Navbar.module.css';
+import About from '../views/About/About';
+import Projects from '../views/Projects/Projects';
+import Skills from '../views/Skills/Skills';
+import darkModeIco from '../../img/dark-mode-ico.svg';
+import lightModeIco from '../../img/light-mode-ico.svg';
 
 const Navbar = (props) => {
     const [pjclass,setPjclass] = useState(false);
     const [skillsclass,setSkillsclass] = useState(false);
     const [abtclass,setAbtclass] = useState(true);
+    const [theme, setTheme] = useState(true);
     
     const onProjectsClicked = () =>{
         props.onViewChange(<Projects />);
@@ -27,6 +30,11 @@ const Navbar = (props) => {
         setSkillsclass(false);
         setAbtclass(true);
     }
+
+    const themeChangeHandler = () => {
+        setTheme((theme) => !theme);
+    }
+    
     return <div className={classes.navbar}>
         <div className={classes.menu}>
             <ul>
@@ -35,6 +43,11 @@ const Navbar = (props) => {
                 <li><button className={abtclass ? classes.selected : ""} onClick={onAboutClicked}>About</button></li>
                 <li><a rel="noreferrer" href="https://drive.google.com/file/d/12ua4B2ktykwoCSIyE5ETORng9NQG3Hr-/view?usp=share_link" target='_blank'><button>Resume</button></a></li>
             </ul>
+        </div>
+        <div className={classes['theme-btn']}>
+            <button onClick={themeChangeHandler}>
+                <img src={theme ? darkModeIco : lightModeIco} alt="" />
+            </button>
         </div>
     </div>
 }
